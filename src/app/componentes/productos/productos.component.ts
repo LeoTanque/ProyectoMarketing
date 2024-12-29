888f7a5f9d5bc0c1984ea4369d83f4f2f7b07533
leodanis <leodanismiranda@gmail.com>
2024-12-29 15:09:56 -0500

    avance

--- src/app/componentes/productos/productos.component.ts
+++ src/app/componentes/productos/productos.component.ts
@@ -6,22 +6,26 @@ import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { DetalleProducto } from '../../clases/detalle-producto';
 import { TableModule } from 'primeng/table';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { SelectModule } from 'primeng/select';
 import { Categoria } from '../../clases/categoria';
 import { Proveedor } from '../../clases/proveedor';
 import { RadioButtonModule } from 'primeng/radiobutton';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-productos',
-  imports: [CommonModule, DialogModule, ButtonModule, TableModule, FormsModule, InputTextModule, CheckboxModule, SelectModule,RadioButtonModule ],
+  imports: [CommonModule, DialogModule, ButtonModule, TableModule, FormsModule, InputTextModule, CheckboxModule,
+     SelectModule,RadioButtonModule, ReactiveFormsModule, ToastModule ],
+     providers: [MessageService],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss'
 })
 export default class ProductosComponent implements OnInit{
 
-
+ // productForm!: FormGroup;
   productoss!:Producto[];
   products!: DetalleProducto[];
   visible: boolean = false;
@@ -29,6 +33,7 @@ export default class ProductosComponent implements OnInit{
   categorias!: Categoria[];
   proveedores!: Proveedor[];
   addProductDialogVisible: boolean = false;
+  submitted: boolean = false;
   newProduct: any = {
     producto_id: '',
     nombre_prod: '',
@@ -47,8 +52,16 @@ export default class ProductosComponent implements OnInit{
   }
 
 
-  constructor(private servicio:ProductoService){}
+  constructor(private fb: FormBuilder,private servicio:ProductoService, private messageService: MessageService){
+
+  }
+
+
   ngOnInit(): void {
+
+
+
+
     this.obtenerProductos();
     this.obtenerProducts();
     this.obtenerCategorias();
@@ -102,25 +115,78 @@ export default class ProductosComponent implements OnInit{
           console.log('Detalles del producto:', this.selectedDetalles);
          } }
 
+
 trackByFn(index: number, item: DetalleProducto): string {
   return item.detalle_producto_id;
  }
 
  openAddProductDialog() {
    this.addProductDialogVisible = true;
+   this.submitted = false;
   }
 
-  saveProduct() {
+  saveProduct1() {
     this.servicio.agregarProducto(this.newProduct).subscribe(
        respuesta => {
          console.log('Producto guardado', respuesta);
          this.obtenerProductos();
          this.addProductDialogVisible = false;
+         this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Producto guardado correctamente.'
+         });
         },
         error => {
           console.error('Error al guardar el producto', error);
+          console.error('Error al guardar el producto', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Hubo un error al guardar el producto.'
+          });
         } );
       }
 
+
+      saveProduct() {
+        this.submitted = true;
+        if (this.newProduct.producto_id &&
+          this.newProduct.nombre_prod &&
+          this.newProduct.marca_prod &&
+          this.newProduct.presentacion_prod &&
+          this.newProduct.categoria.categoria_id &&
+          this.newProduct.proveedor.proveedor_id) {
+             this.servicio.agregarProducto(this.newProduct).subscribe(
+               respuesta => {
+                 console.log('Producto guardado', respuesta);
+                 this.messageService.add({
+                  severity: 'success',
+                  summary: 'Éxito',
+                  detail: 'Producto guardado correctamente.'
+                });
+                this.obtenerProductos();
+                this.addProductDialogVisible = false;
+              }, error => {
+                 console.error('Error al guardar el producto', error);
+                 this.messageService.add({
+                  severity: 'error',
+                  summary: 'Error',
+                  detail: 'Hubo un error al guardar el producto.'
+                });
+              } );
+            } else {
+              this.messageService.add({
+                severity: 'warn',
+                summary: 'Advertencia',
+                detail: 'Debe llenar todos los campos obligatorios.'
+              });
+            }
+          }
+
+      hideDialog() {
+       this.addProductDialogVisible= false
+       this.submitted = false;
+      }
 
 }
